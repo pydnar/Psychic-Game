2c53158b062e03eaf4876d29e77df68cf2d8df39
212680803 <randy.porter1@ge.com>
2018-08-01 15:53:54 -0400

    Second push, change to files.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,63 @@
 // Options up for random selection by Computer.
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// gameSetup function
-let guessesLeft = 9;
+// game vars
+var guessesLeft = 9;
 var wins = 0;
 var losses = 0;
-var userattempts = 0;
 var guesses = [];
 
-//Lets the computer select a random letter from the available choices
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// updateScoreBoard called upon on a Win or Loss.
 
-//Allows the user 9 guesses
-// guesses = guesses || 9
-function updateGuessesLeft() {
-    // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
-    document.querySelector('#guesses-left').innerHTML = "Guesses left: " + guessesLeft;
-};
 
-function updateLetterToGuess() {
-    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-};
-
-function updateGuessesSoFar() {
-    // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
-    document.querySelector('#guesses').innerHTML = "Your Guesses so far: " + guesses.join(', ');
-};
-// Function will be called when we reset everything
-var reset = function() {
-    totalGuesses = 9;
-    guessesLeft = 9;
-    guesses = [];
-
-    updateLetterToGuess();
-    updateGuessesLeft();
-    updateGuessesSoFar();
+// UpdateGuesses, called upon when userGuess != ComputerGuess
+function updateGuesses() {
+    guessesLeft--;
+    document.getElementById("guesses-left").textContent = "Guesses Left: " + guessesLeft;
 }
 
-updateLetterToGuess();
-updateGuessesLeft();
+function updateLoss() {
+    losses++;
+    document.getElementById("losses").textContent = "Your Losses so far: " + losses;
+}
 
-//When key is released it becomes the users guess
-document.onkeyup = function(event) {
+function updateWins() {
+    wins++;
+    document.getElementById("wins").textContent = "Your Wins so far: " + wins;
+}
+
+function updateScoreBoard() {
+    var guessesLeft = 9;
+    document.getElementById("guesses").textContent = "Your Guesses: ";
+    document.getElementById("guesses-left").textContent = "Guesses Left: " + guessesLeft;
+}
+
+// reset called upon on a Win or Loss.
+function reset() {
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    updateScoreBoard();
+}
+
+//Lets the computer select a random letter from the available choices
+document.onkeyup = function (event) {
+    //computerGuess randomizer.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    //userGuess capture keypress.
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    var check = computerChoices.includes(userGuess);
-
-    if (check === false) {
-        alert("That was not a valid guess, try again?");
-        return false;
-    } else if (check === true) {
-        //If the Users choice was an alphabet char then update guesses left and add users guess to the array of guessed letters
-        guessesLeft--;
-        guesses.push(userGuess);
-        updateGuessesLeft();
-        updateGuessesSoFar();
-
-        if (guessesLeft > 0) {
-            if (userGuess == letterToGuess) {
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                userGuess = userGuess.toUpperCase();
-                alert("Yes, you are psychic! Mantis has chosen " + userGuess);
-                reset();
-            }
-        } else if (guessesLeft == 0) {
-            // Then we will loss and we'll update the html to display the loss 
-            losses++;
-            boo.play();
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Sorry, you're not psychic, maybe try again?");
-            // Then we'll call the reset. 
+    //update list of user guesses.
+    guesses.push(userGuess);
+    document.getElementById("guesses").textContent = "Your Guesses: " + guesses.join(', ');
+    //deincrement guesses using our updateGuesses func.
+    updateGuesses();
+    if (guessesLeft > 0) {
+        if (userGuess == computerGuess) {
+            updateWins();
             reset();
         }
-        return false;
-    } else {
-        alert("Oops, we have an error");
+        if (guessesLeft < 1) {
+            updateLoss();
+            alert("YOU HAVE LOST A GAME. Incrementing loses by ONE point.")
+            reset();
+        }
     }
-
-};
+}
